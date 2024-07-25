5c1d69eb6c890fd87a59dacf4eabff1506c16a3f
ckstnr99 <ckstnr99@naver.com>
2024-07-25 11:51:04 +0900

    2024.07.25. 11:50 sql

--- Join_Sub.sql
+++ Join_Sub.sql
@@ -102,6 +102,72 @@ FROM employee E INNER JOIN department D
 ON E.department_code = D.department_code
 WHERE D.name = '영업부';
 
+-- 서브쿼리 : 쿼리 내부에 존재하는 또 다른 쿼리, 쿼리 결과를 조건이나 테이블로 사용할 수 있도록 함
+
+-- WHERE 절에서 서브쿼리 : 조회 결과를 조건으로 사용하여 조건을 동적으로 지정할 수 있도록 함
+-- WHERE 절에서 비교연산 등으로 사용할 때 조회하는 컬럼의 개수 및 레코드의 개수 주의!
+
+-- 부서명이 '영업부'인 사원에 대해
+-- 사번, 이름, 나이를 조회하시오. 
+SELECT employee_number, name, age
+FROM employee
+WHERE department_code = (
+	SELECT department_code FROM department
+    WHERE name = '영업부'
+);
+
+SELECT employee_number, name, age
+FROM employee
+WHERE department_code = (
+	SELECT * FROM department
+    WHERE name = '영업부'
+); # error
+
+SELECT employee_number, name, age
+FROM employee
+WHERE department_code = (
+	SELECT department_code FROM department
+); # error
+
+SELECT employee_number, name, age
+FROM employee
+WHERE department_code IN (
+	SELECT department_code FROM department
+);
+
+SELECT employee_number, name, age
+FROM employee
+WHERE department_code IN (
+	SELECT * FROM department
+); # error
+
+-- EX
+SELECT employee_number, name, age FROM employee
+WHERE department_code = ('여기 안으로');
+-- +
+SELECT department_code FROM department
+WHERE name = '영업부';
+
+-- FROM 절에서 서브쿼리 : 조회 결과 테이블을 다시 FROM 절에서 재사용
+-- 서브쿼리를 FROM 절에서 사용할 땐 반드시 별칭을 사용해야 함!
+SELECT * FROM department
+WHERE name = '영업부';
+
+SELECT E.employee_number '사번', E.name '이름', E.age '나이'
+FROM employee E INNER JOIN (
+	SELECT * FROM department
+	WHERE name = '영업부'
+) D
+ON E.department_code = D.department_code;
+
+SELECT * 
+FROM (
+	SELECT * FROM department
+) D
+WHERE name = '영업부';
+
+
+
 
 
 
